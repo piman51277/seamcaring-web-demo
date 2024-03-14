066a7743b45fd859c23700f055f3d9f4621ed480
piman51277 <piman.dev@gmail.com>
2024-03-14 07:24:20 -0400

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist", "client"),
+    path: path.resolve(__dirname, "assets", "js"),
   },
   mode: "production",
+  devtool: false
 };
