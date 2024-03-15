f850ae86b4af231b4975e43779b07de51cb72f2c
piman51277 <piman.dev@gmail.com>
2024-03-15 09:26:53 -0400

    fix pixel order

--- src/client/image/bufToImage.ts
+++ src/client/image/bufToImage.ts
@@ -1,5 +1,3 @@
-import { MarvinImage } from "marvinj-ts";
-
 /**
  * Create an image from a buffer
  *
@@ -12,16 +10,17 @@ export function bufToImage(
   width: number,
   height: number,
   pixels: Uint32Array
-): MarvinImage {
-  const img = new MarvinImage(width, height);
-  const data = img.data;
-  for (let pix = 0; pix < width * height; pix++) {
-    const offset = pix * 4;
-    const value = pixels[pix];
-    data[offset] = (value >> 24) & 0xff;
-    data[offset + 1] = (value >> 16) & 0xff;
-    data[offset + 2] = (value >> 8) & 0xff;
-    data[offset + 3] = value & 0xff;
+): ImageData {
+  const imgData = new ImageData(width, height);
+
+  //convert to RGBA Uint8Clamped
+  for (let i = 0; i < pixels.length; i++) {
+    const pixel = pixels[i];
+    imgData.data[i * 4 + 0] = (pixel >> 24) & 0xff;
+    imgData.data[i * 4 + 1] = (pixel >> 16) & 0xff;
+    imgData.data[i * 4 + 2] = (pixel >> 8) & 0xff;
+    imgData.data[i * 4 + 3] = pixel & 0xff;
   }
-  return img;
+
+  return imgData;
 }
